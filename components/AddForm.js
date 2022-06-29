90da37e473bc652c345093e57a2653eb93903d07
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-29 16:25:40 -0700

    Implement form that adds new line items
    Renamed some stuff too in order to avoid item.item code.

---
+++ components/AddForm.js
@@ -0,0 +1,13 @@
+export default function createAddForm(form, { handleAddLineItem }) {
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const formData = new FormData(form);
+        handleAddLineItem({
+            item: formData.get('item'),
+            quantity: formData.get('quantity'),
+        });
+    });
+
+    return () => {};
+}
--- components/List.js
+++ components/List.js
@@ -2,16 +2,16 @@ export default function createList(root) {
     return ({ list }) => {
         root.innerHTML = '';
 
-        for (const item of list) {
-            root.append(createListItem(item));
+        for (const lineItem of list) {
+            root.append(createListItem(lineItem));
         }
     };
 }
 
-function createListItem(item) {
+function createListItem(lineItem) {
     const div = document.createElement('div');
     const p = document.createElement('p');
-    p.textContent = `${item.quantity} ${item.item}`;
+    p.textContent = `${lineItem.quantity} ${lineItem.item}`;
 
     div.append(p);
     return div;
