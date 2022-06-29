1f7e7f04426e9255262ad7c4ad9e8ee55e0ff554
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-29 12:51:04 -0700

    Use my supabase project url and key

--- services/client.js
+++ services/client.js
@@ -1,5 +1,4 @@
-const SUPABASE_URL = 'https://nwxkvnsiwauieanvbiri.supabase.co';
-const SUPABASE_KEY =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzAwMzQzNCwiZXhwIjoxOTUyNTc5NDM0fQ.8XIsU0FANdaNeQnT-DojpTL-GTlTPZ4CYZDEetpFpWc';
+const SUPABASE_URL = 'https://vzzttwritrdyvrtauwik.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6enR0d3JpdHJkeXZydGF1d2lrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTY1MzAxNDAsImV4cCI6MTk3MjEwNjE0MH0.OZoBLEtI2H0xc6S1cVvcaB5oNbZjREq_ykDTRnnhZGc';
 
 export const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
