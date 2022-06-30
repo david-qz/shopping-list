ba1ff318ea7d74e6291b2a5fe2025f7b1e8b212a
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-29 18:00:34 -0700

    Add ability to mark items as bought

--- services/list-service.js
+++ services/list-service.js
@@ -29,6 +29,21 @@ export async function createLineItem(item) {
     return data;
 }
 
+export async function updateLineItem(item) {
+    const { data, error } = await client
+        .from(TABLE)
+        .update(item)
+        .match({ id: item.id })
+        .single();
+
+    if (error) {
+        logError('updateLineItem()', error);
+        return null;
+    }
+
+    return data;
+}
+
 function logError(context, error) {
     // eslint-disable-next-line no-console
     console.log(`${context}: ${error.message}`);
